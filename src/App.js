cbea76614119ad52308561743822e630eb0e2a68
unknown <ssintern50@gmail.com>
2024-01-28 16:32:51 +0530

    Add my files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+      <header className="header">
+      <img src={process.env.PUBLIC_URL + '/logo.png'} alt="Company Logo" className="logo" />
+        <div className="text-container">
+          <h1>Hello from StateStreet Interns</h1>
+          <p>Karan & Seshank</p>
+        </div>
       </header>
     </div>
   );
